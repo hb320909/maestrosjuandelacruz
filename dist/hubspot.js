a0df72e4a12b21f96b5e1ed50d7630921808e312
harry <hbro319@iesalbox.com>
2026-02-08 15:06:43 +0100

    Add app.js and HTML setup with Supabase integration

--- dist/hubspot.js
+++
@@ -1,9 +0,0 @@
-fetch("https://api.hubapi.com/crm/v3/objects/contacts", {
-  headers: {
-    Authorization: "Bearer eu1-737e-edb3-4b6d-8532-23b36ea925f7"
-  }
-})
-.then(res => res.json())
-.then(data => {
-  console.log(data);
-});
